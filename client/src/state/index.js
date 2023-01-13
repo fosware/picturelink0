012e578258ba096f360f89aec637d91ce1789bfc
fosahadal <fosahadal@protonmail.com>
2023-01-13 14:00:29 -0600

    starting client

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -43,5 +43,5 @@ export const authSlice = createSlice({
     }
 })
 
-export const { setMode, setLogin, setFriends, setPost, setPosts } = authSlice.actions;
+export const { setMode, setLogin, setLogout, setFriends, setPost, setPosts } = authSlice.actions;
 export default authSlice.reducer;
